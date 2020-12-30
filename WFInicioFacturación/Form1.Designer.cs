4df6256bca3946c02c555a3584954f4c6f17268e
NRamosD <nixonramosdue@gmail.com>
2020-12-30 11:13:15 -0500

    Commit de inicio de proyecto

--- WFInicioFacturación/Form1.Designer.cs
+++ WFInicioFacturación/Form1.Designer.cs
@@ -28,92 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Aqua;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.button1.Location = new System.Drawing.Point(56, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(293, 205);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "wiwi";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(456, 54);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(407, 247);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(256, 28);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "NO SE GUARDA MRDA";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(31, 348);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(293, 28);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Para comprobar los merge";
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Lime;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.Red;
-            this.button2.Location = new System.Drawing.Point(433, 322);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(167, 82);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "CHEBERE";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(713, 452);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- WFInicioFacturación/Form1.cs
+++ WFInicioFacturación/Form1.cs
@@ -12,28 +12,11 @@ namespace WFInicioFacturación
 {
     public partial class Form1 : Form
     {
-        string unavariable;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            button1.Text = "No me clickees";
-
-            label1.Text = "que friegas";
-            label1.BackColor = Color.AntiqueWhite;
-            button2.Text = "Escribe bien\npendejo";
-            button2.TextAlign = ContentAlignment.MiddleCenter;
-            label2.Text = "NO SE GUARDA MRDA";
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            label2.Text = "Si se guardó we";
-            unForm jaja = new unForm();
-            jaja.Show();
-        }
     }
 }
