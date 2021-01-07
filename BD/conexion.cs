1e5457ffe7207fe8950e89d526ee6605726beb60
NRamosD <nixonramosdue@gmail.com>
2021-01-06 22:48:55 -0500

    Bibliotecas de clases listas

--- BD/conexion.cs
+++ BD/conexion.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace BD
 {
     class conexion
     {
+        public string cadena = "Data Source = NR; Initial Catalog = sqlserver; Integrated Security = true";
+        public SqlConnection conectar = new SqlConnection();
+
+        public conexion()
+        {
+            conectar.ConnectionString = cadena;
+        }
+
+        public void abrir()
+        {
+            try
+            {
+                conectar.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al conectarse.\n" + e);
+            }
+        }
+        public void cerrar()
+        {
+            conectar.Close();
+        }
     }
 }
--- Logica/OperacionesLogicas.cs
+++ Logica/OperacionesLogicas.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//using BD;
+using System.Data;
+
 namespace Logica
 {
     class OperacionesLogicas
     {
+        /*
+        private Operaciones objOpe = new Operaciones();
+
+        public DataTable mostrarDatos()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objOpe.mostrar();
+            return tabla;
+        }
+        public void Insertar(string id, string nom)
+        {
+            objOpe.Insertar(id, nom);
+        }
+        public void Eliminar(string id)
+        {
+            objOpe.Eliminar(id);
+        }*/
     }
 }
