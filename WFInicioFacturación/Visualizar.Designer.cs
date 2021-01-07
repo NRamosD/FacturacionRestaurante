2402df4e6bfb9b3aa1246db5cb868ffde296b8aa
CRISTIAN PATRICIO MONTESDEOCA VITERI <titian1105@gmail.com>
2021-01-06 21:00:40 -0500

    crud:_cl

---
+++ WFInicioFacturación/Visualizar.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WFInicioFacturación
+{
+    partial class Visualizar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbTelef = new System.Windows.Forms.TextBox();
+            this.txbDireecion = new System.Windows.Forms.TextBox();
+            this.txbID = new System.Windows.Forms.TextBox();
+            this.txbApellido = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbCod = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAceptar = new FontAwesome.Sharp.IconButton();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(111, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 24);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Dirección";
+            // 
+            // txbTelef
+            // 
+            this.txbTelef.BackColor = System.Drawing.Color.White;
+            this.txbTelef.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbTelef.Enabled = false;
+            this.txbTelef.Font = new System.Drawing.Font("Rockwell", 12F);
+            this.txbTelef.Location = new System.Drawing.Point(278, 337);
+            this.txbTelef.Name = "txbTelef";
+            this.txbTelef.Size = new System.Drawing.Size(180, 24);
+            this.txbTelef.TabIndex = 40;
+            // 
+            // txbDireecion
+            // 
+            this.txbDireecion.BackColor = System.Drawing.Color.White;
+            this.txbDireecion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbDireecion.Enabled = false;
+            this.txbDireecion.Font = new System.Drawing.Font("Rockwell", 12F);
+            this.txbDireecion.Location = new System.Drawing.Point(278, 279);
+            this.txbDireecion.Name = "txbDireecion";
+            this.txbDireecion.Size = new System.Drawing.Size(180, 24);
+            this.txbDireecion.TabIndex = 41;
+            // 
+            // txbID
+            // 
+            this.txbID.BackColor = System.Drawing.Color.White;
+            this.txbID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbID.Enabled = false;
+            this.txbID.Font = new System.Drawing.Font("Rockwell", 12F);
+            this.txbID.Location = new System.Drawing.Point(278, 218);
+            this.txbID.Name = "txbID";
+            this.txbID.Size = new System.Drawing.Size(180, 24);
+            this.txbID.TabIndex = 42;
+            // 
+            // txbApellido
+            // 
+            this.txbApellido.BackColor = System.Drawing.Color.White;
+            this.txbApellido.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbApellido.Enabled = false;
+            this.txbApellido.Font = new System.Drawing.Font("Rockwell", 12F);
+            this.txbApellido.Location = new System.Drawing.Point(278, 159);
+            this.txbApellido.Name = "txbApellido";
+            this.txbApellido.Size = new System.Drawing.Size(180, 24);
+            this.txbApellido.TabIndex = 43;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.BackColor = System.Drawing.Color.White;
+            this.txbNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbNombre.Enabled = false;
+            this.txbNombre.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNombre.Location = new System.Drawing.Point(278, 104);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(180, 24);
+            this.txbNombre.TabIndex = 44;
+            // 
+            // txbCod
+            // 
+            this.txbCod.BackColor = System.Drawing.Color.White;
+            this.txbCod.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbCod.Enabled = false;
+            this.txbCod.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbCod.Location = new System.Drawing.Point(278, 47);
+            this.txbCod.Name = "txbCod";
+            this.txbCod.Size = new System.Drawing.Size(180, 24);
+            this.txbCod.TabIndex = 45;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(111, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 24);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Teléfono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(111, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 24);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Apellido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(111, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 24);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(111, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 24);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(111, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Código ";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(172)))), ((int)(((byte)(100)))));
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(214)))), ((int)(((byte)(177)))));
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.IconChar = FontAwesome.Sharp.IconChar.UserCheck;
+            this.btnAceptar.IconColor = System.Drawing.Color.Black;
+            this.btnAceptar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnAceptar.IconSize = 35;
+            this.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAceptar.Location = new System.Drawing.Point(222, 377);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(123, 36);
+            this.btnAceptar.TabIndex = 33;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Visible = false;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.Transparent;
+            this.iconButton1.Enabled = false;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.AddressBook;
+            this.iconButton1.IconColor = System.Drawing.Color.DimGray;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.iconButton1.IconSize = 350;
+            this.iconButton1.Location = new System.Drawing.Point(21, 41);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(512, 392);
+            this.iconButton1.TabIndex = 46;
+            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Visible = false;
+            // 
+            // Visualizar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(550, 459);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbTelef);
+            this.Controls.Add(this.txbDireecion);
+            this.Controls.Add(this.txbID);
+            this.Controls.Add(this.txbApellido);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.txbCod);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iconButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Visualizar";
+            this.Text = "Visualizar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private FontAwesome.Sharp.IconButton btnAceptar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbTelef;
+        private System.Windows.Forms.TextBox txbDireecion;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.TextBox txbApellido;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox txbCod;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
